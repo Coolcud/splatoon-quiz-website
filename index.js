b76f9f769935a5b4c096ccc3db7ed2ee19f76780
Sage Choi <coolcuds@gmail.com>
2023-07-10 16:48:50 -0700

    Change .answers to .options and remove IIFE

--- index.js
+++ index.js
@@ -1,106 +1,103 @@
-(function() {
-  const quizContainer = document.getElementById('quiz')
-  const submitButton = document.getElementById('submit')
-  const resultsContainer = document.getElementById('results')
+const quizContainer = document.getElementById('quiz')
+const resultsContainer = document.getElementById('results')
+const submitButton = document.getElementById('submit')
 
-  const quizQuestions = [
-    {
-      question: "Which of the following characters is NOT part of Deep Cut?",
-      options: {
-        a: "Big Man",
-        b: "Judd",
-        c: "Shiver",
-        d: "Frye",
-      },
-      correctAnswer: "b"
+const quizQuestions = [
+  {
+    question: "Which of the following characters is NOT part of Deep Cut?",
+    options: {
+      a: "Big Man",
+      b: "Judd",
+      c: "Shiver",
+      d: "Frye"
     },
-    {
-      question: "How many splatfests have there been in North America?",
-      options: {
-        a: "57",
-        b: "51",
-        c: "50",
-        d: "55",
-      },
-      correctAnswer: "d"
+    correctAnswer: "b"
+  },
+  {
+    question: "How many splatfests have there been in North America?",
+    options: {
+      a: "57",
+      b: "51",
+      c: "50",
+      d: "55"
     },
-    {
-      question: "How many ability chunks does it cost for 3 secondary ability slots?",
-      options: {
-        a: "Splatoon 1",
-        b: "Splatoon 2",
-        c: "Splatoon 3",
-      },
-      correctAnswer: "a"
-    }
-  ];
+    correctAnswer: "d"
+  },
+  {
+    question: "In which game was the ability 'Ink Resistance Up' introduced?",
+    options: {
+      a: "Splatoon 1",
+      b: "Splatoon 2",
+      c: "Splatoon 3"
+    },
+    correctAnswer: "a"
+  }
+];
 
-  function buildQuiz() {
-    const output = [];
+function buildQuiz() {
+  const output = [];
 
-    // For each quiz question, build the HTML
-    quizQuestions.forEach((currentQuestion, questionNumber) => {
-      const answers = [];
+  // For each quiz question, build the HTML
+  quizQuestions.forEach((currentQuestion, questionNumber) => {
+    const answers = [];
 
-      for (option in currentQuestion.answers) {
-        // Add HTML radio button
-        answers.push(
-          `
-          <label>
-            <input
-              type="radio"
-              name="question${questionNumber}"
-              value="${option}"
-            />
-            ${option} :
-            ${currentQuestion.answers[option]}
-          </label>
-          `
-        );
-      };
-
-      // Add current question and options to output
-      output.push(
+    for (option in currentQuestion.options) {
+      // Add HTML radio button
+      answers.push(
         `
-        <div class="question">${question.question}</div>
-        <div class="answers">${answers.join('')}</div>
+        <label>
+          <input
+            type="radio"
+            name="question${questionNumber}"
+            value="${option}"
+          />
+          ${option}: ${currentQuestion.options[option]}
+        </label>
         `
       );
-    });
+    }
 
-    // Combine output list into string of HTML and add to the page
-    quizContainer.innerHTML = output.join('');
-  }
+    // Add current question and options to output
+    output.push(
+      `
+      <div class="question">${currentQuestion.question}</div>
+      <div class="answers">${answers.join('')}</div>
+      `
+    );
+  });
 
-  function showResults() {
-    // Retrieve answer containers from quiz
-    const answerContainers = quizContainer.querySelectorAll('.answers');
+  // Combine output list into string of HTML and add to the page
+  quizContainer.innerHTML = output.join('');
+}
 
-    // Track correct user guesses
-    let numCorrectGuesses = 0;
+function showResults() {
+  // Retrieve answer containers from quiz
+  const answerContainers = quizContainer.querySelectorAll('.answers');
 
-    quizQuestions.forEach((currentQuestion, questionNumber) => {
-      // Retrieve user's answer choice
-      const answerContainer = answerContainers[questionNumber];
-      const selector = `input[name=question${questionNumber}]:checked`;
-      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+  // Track correct user guesses
+  let numCorrectGuesses = 0;
 
-      // Right and wrong answers behavior
-      if (userAnswer === currentQuestion.correctAnswer) {
-        numCorrectGuesses++; // +1 to correct answers total
-        answerContainers[questionNumber].style.color = 'lightgreen';
-      } else {
-        answerContainers[questionNumber].style.color = 'red';
-      };
-    });
+  quizQuestions.forEach((currentQuestion, questionNumber) => {
+    // Retrieve user's answer choice
+    const answerContainer = answerContainers[questionNumber];
+    const selector = `input[name=question${questionNumber}]:checked`;
+    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
 
-    // Display correct answers out of total
-    resultsContainer.innerHTML = ` You got ${numCorrectGuesses} out of ${quizQuestions.length}!`;
-  }
+    // Right and wrong answers behavior
+    if (userAnswer === currentQuestion.correctAnswer) {
+      numCorrectGuesses++; // +1 to correct guesses total
+      answerContainers[questionNumber].style.color = 'lightgreen';
+    } else {
+      answerContainers[questionNumber].style.color = 'red';
+    };
+  });
 
-  // Display quiz
-  buildQuiz();
+  // Display correct answers out of total
+  resultsContainer.innerHTML = ` You got ${numCorrectGuesses} out of ${quizQuestions.length}!`;
+}
 
-  // Show quiz results on submit
-  submitButton.addEventListener('click', showResults);
-})();
+// Display quiz
+buildQuiz();
+
+// Show quiz results on submit
+submitButton.addEventListener('click', showResults);
